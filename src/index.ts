d135e75fc7094f0e2f92cd6c59fc0b8fc1c33c21
Dou Du <dou.du.epfl@gmail.com>
2020-01-29 19:57:50 +0100

    Make the sidebar working

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import {
-  JupyterFrontEnd, JupyterFrontEndPlugin
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin,
+  ILabShell
 } from '@jupyterlab/application';
 
+import { Widget } from '@phosphor/widgets';
+
+import '../style/index.css';
 
 /**
  * Initialization data for the osscar-labextensions extension.
@@ -9,9 +14,32 @@ import {
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'osscar-labextensions',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension osscar-labextensions is activated!');
-  }
+  activate: activate,
+  requires: [
+    ILabShell
+  ]
 };
+
+function activate(
+  app: JupyterFrontEnd,
+  labShell: ILabShell): void {
+
+    const sidebar = new DouSidebar();
+
+    sidebar.id = 'testid';
+    sidebar.title.iconClass = 'dou-DaskLogo jp-SideBar-tabIcon';
+    sidebar.title.caption = 'OSSCAR';
+
+
+    labShell.add(sidebar, 'left', { rank: 600 });
+
+  }
+
+export class DouSidebar extends Widget{
+  constructor(){
+    super();
+    this.addClass('dou-DaskSidebar');
+  }
+}
 
 export default extension;
