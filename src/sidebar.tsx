fad248973b30c130e7dca8f8c8dfd2413722ae62
Dou <uu.dou.du@gmail.com>
2020-01-31 00:20:56 +0100

    Add a button to the panelout

--- src/sidebar.tsx
+++ src/sidebar.tsx
@@ -1,9 +1,27 @@
 import { Widget, PanelLayout } from '@phosphor/widgets';
 
+import { ReactWidget } from "@jupyterlab/apputils";
+
+import * as React from "react";
+
 export class chemSidebar extends Widget{
   constructor(){
     super();
     this.addClass('dou-DaskSidebar');
-    this.layout = new PanelLayout();
+    let layout = (this.layout = new PanelLayout());
+
+    const renderOnSaveCheckbox = ReactWidget.create(
+    <input 
+      className = "pt_button"
+      type = "button"
+      value="Open periodic table"
+      onClick={() => {
+        console.log("test is working.");
+      }}
+    />
+)
+
+    // renderOnSaveCheckbox.add("pt_button");
+    layout.addWidget(renderOnSaveCheckbox);
   }
 }
