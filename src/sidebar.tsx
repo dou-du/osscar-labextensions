33ccee6be245d7ad4244094d3e65266a63a56f9c
Dou Du <dou.du.epfl@gmail.com>
2020-01-31 17:44:53 +0100

    Add the code-input-widget for the OSSCAR

--- src/sidebar.tsx
+++ src/sidebar.tsx
@@ -19,6 +19,7 @@ export class chemSidebar extends Widget{
     this._notebookTracker = notebookTracker;
 
     const renderPeriodicTable = ReactWidget.create(
+      <div>
     <input
       className = "pt_button"
       type = "button"
@@ -48,40 +49,72 @@ export class chemSidebar extends Widget{
         console.log(this._notebookTracker.size + "test is working. &&&");
       }}
     />
-)
 
-const renderJmol = ReactWidget.create(
-<input
-  className = "jmol_button"
-  type = "button"
-  // value="Open periodic table"
-  draggable = {true}
-  onDragOver = {ev => {ev.preventDefault()}}
-  onDragStart={ev => ev.dataTransfer.setData("text", 'from jmolwidgets import WidgetJmol\njmol = WidgetJmol()\ndisplay(jmol)')}
-  onClick={() => {
-    const current = this._notebookTracker.currentWidget;
+    <input
+      className = "jmol_button"
+      type = "button"
+      // value="Open periodic table"
+      draggable = {true}
+      onDragOver = {ev => {ev.preventDefault()}}
+      onDragStart={ev => ev.dataTransfer.setData("text", 'from jmolwidgets import WidgetJmol\njmol = WidgetJmol()\ndisplay(jmol)')}
+      onClick={() => {
+        const current = this._notebookTracker.currentWidget;
 
-    if (current) {
-      // return NotebookActions.hideCode(current.content);
-      // return current.content.widgets[0].hide();
+        if (current) {
+          // return NotebookActions.hideCode(current.content);
+          // return current.content.widgets[0].hide();
 
-      current.content.widgets.forEach(cell => {
-        if (
-          current.content.isSelectedOrActive(cell) &&
-          cell.model.type === 'code'
-        ) {
-          cell.model.value.text = 'from jmolwidgets import WidgetJmol\njmol = WidgetJmol()\ndisplay(jmol)'; 
-          CodeCell.execute(cell as CodeCell, current.session);
-          console.log(cell.model.value.text);
+          current.content.widgets.forEach(cell => {
+            if (
+              current.content.isSelectedOrActive(cell) &&
+              cell.model.type === 'code'
+            ) {
+              cell.model.value.text = 'from jmolwidgets import WidgetJmol\njmol = WidgetJmol()\ndisplay(jmol)';
+              CodeCell.execute(cell as CodeCell, current.session);
+              console.log(cell.model.value.text);
+            }
+          });
         }
-      });
-    }
-  }}
-/>
+      }}
+    />
+
+    <input
+      className = "code_button"
+      type = "button"
+      // value="Open periodic table"
+      draggable = {true}
+      onDragOver = {ev => {ev.preventDefault()}}
+      onDragStart={ev => ev.dataTransfer.setData("text", 'from widget_code_input import WidgetCodeInput\n'+
+      'code_widget = WidgetCodeInput(function_name="my_example_code")\n' +
+      'display(code_widget)')}
+      onClick={() => {
+        const current = this._notebookTracker.currentWidget;
+
+        if (current) {
+          // return NotebookActions.hideCode(current.content);
+          // return current.content.widgets[0].hide();
+
+          current.content.widgets.forEach(cell => {
+            if (
+              current.content.isSelectedOrActive(cell) &&
+              cell.model.type === 'code'
+            ) {
+              cell.model.value.text = 'from widget_code_input import WidgetCodeInput\n'+
+              'code_widget = WidgetCodeInput(function_name="my_example_code")\n' +
+              'display(code_widget)';
+              CodeCell.execute(cell as CodeCell, current.session);
+              console.log(cell.model.value.text);
+            }
+          });
+        }
+      }}
+    />
+    </div>
 )
+
+
 
     // renderOnSaveCheckbox.add("pt_button");
     layout.addWidget(renderPeriodicTable);
-    layout.addWidget(renderJmol);
   }
 }
